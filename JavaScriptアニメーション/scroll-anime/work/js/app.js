e604ca67b833f5447ea8aefe3e9392410b7f659c
rihokegai <choisi.come20@gmail.com>
2025-11-27 21:11:31 +0900

    アニメーション課題

--- JavaScriptアニメーション/scroll-anime/work/js/app.js
+++ JavaScriptアニメーション/scroll-anime/work/js/app.js
@@ -1,9 +1,11 @@
 const boxes = document.querySelectorAll(".scroll-in");
-const callback = (entries,observer) => {
+const callback = (entries) => {
     entries.forEach(entry => {
         if(entry.isIntersecting){
-            entry.target.classList.add("displayed")
-        } 
+            entry.target.classList.add("displayed");
+        } else {
+            entry.target.classList.remove("displayed");
+        }
         
     });
 };
